8824140b7e298979f60f7a12fe564ca957af0a54
Mohammed Raiyan <mohammedraiyan772@gmail.com>
2026-09-23 11:28:46 +0530

    added

---
+++ demo.py
@@ -0,0 +1,8 @@
+def hello():
+    """print("hello guys")print("new updates")"""
+    return ("hello")
+    x = 7
+    if x < 5:
+        print("x is less than 5")
+    else:
+        print("x is greater than 5")
